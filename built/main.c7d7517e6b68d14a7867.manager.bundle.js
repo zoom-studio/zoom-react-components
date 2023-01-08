a65d2bc39429c1e5a6cf4c58a1312a7eccfc82b8
qafoori <qafoori.hr@gmail.com>
2023-01-08 18:38:05 +0330

    feat(global): Configure github action

---
+++ built/main.c7d7517e6b68d14a7867.manager.bundle.js
@@ -0,0 +1,25 @@
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [2],
+  {
+    509: function (module, exports, __webpack_require__) {
+      __webpack_require__(510),
+        __webpack_require__(934),
+        __webpack_require__(938),
+        __webpack_require__(939),
+        __webpack_require__(935),
+        __webpack_require__(932),
+        __webpack_require__(931),
+        __webpack_require__(933),
+        __webpack_require__(930),
+        __webpack_require__(936),
+        __webpack_require__(937),
+        __webpack_require__(928),
+        __webpack_require__(871),
+        __webpack_require__(908),
+        __webpack_require__(929),
+        (module.exports = __webpack_require__(940))
+    },
+    556: function (module, exports) {},
+  },
+  [[509, 3, 4]],
+])

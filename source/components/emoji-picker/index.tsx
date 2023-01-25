e30c42611b72d1c412baf3e0e5eea2bc1320ca80
qafoori <qafoori.hr@gmail.com>
2023-01-26 01:20:46 +0330

    feat(emoji-picker): Draft

--- source/components/emoji-picker/index.tsx
+++ source/components/emoji-picker/index.tsx
@@ -1,9 +1,22 @@
 import React, { FC, HTMLAttributes } from 'react'
 import { useZoomComponent } from '../../hooks'
+import { EmojiNS } from '../emoji'
 
 export namespace EmojiPickerNs {
   export interface Props {
     containerProps?: HTMLAttributes<HTMLDivElement>
+  }
+
+  export type I18n = {
+    collections: {
+      [name in EmojiNS.Emojis.GroupNames]: string
+    }
+    groups: {
+      [name in EmojiNS.Emojis.SubgroupNames]: string
+    }
+    emojis: {
+      [name in EmojiNS.Emojis.Names]: string
+    }
   }
 }
 
@@ -11,6 +24,16 @@ export const EmojiPicker: FC<EmojiPickerNs.Props> = ({ containerProps }) => {
   const { createClassName } = useZoomComponent('emoji-picker')
 
   const containerClasses = createClassName(containerProps?.className)
+
+  // const getCollectionName = (collection: EmojiNS.Emojis.GroupNames): string => {
+  //   return i18n?.emojiPicker?.collections?.[collection] ?? collection
+  // }
+  // const getGroupName = (group: EmojiNS.Emojis.SubgroupNames): string => {
+  //   return i18n?.emojiPicker?.groups?.[group] ?? group
+  // }
+  // const getEmojiName = (emojiName: EmojiNS.Emojis.Names): string => {
+  //   return i18n?.emojiPicker?.emojis?.[emojiName] ?? emojiName
+  // }
 
   return <div className={containerClasses}>emoji picker</div>
 }
--- source/components/zoom-log-provider/index.tsx
+++ source/components/zoom-log-provider/index.tsx
@@ -2,7 +2,7 @@ import React, { createContext, FC, ReactNode } from 'react'
 
 import { logs } from '../../constants'
 
-import { ImageViewerNS } from '..'
+import { ImageViewerNS, EmojiPickerNs } from '..'
 
 export namespace ZoomLogProviderNS {
   export type Error = string | object
@@ -21,6 +21,7 @@ export namespace ZoomLogProviderNS {
     }
     globalI18ns?: {
       imageViewer?: ImageViewerNS.I18n
+      emojiPicker?: EmojiPickerNs.I18n
     }
   }
 }

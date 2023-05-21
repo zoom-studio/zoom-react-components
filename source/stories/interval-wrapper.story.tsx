14d3d1e91d725af81846e8d89f773aa1c7eb06bf
qafoori <qafoori.hr@gmail.com>
2023-05-22 01:33:27 +0330

    feat(interval-wrapper): Write more stories for interval wrapper component

--- source/stories/interval-wrapper.story.tsx
+++ source/stories/interval-wrapper.story.tsx
@@ -3,8 +3,16 @@ import React, { FC } from 'react'
 import { ComponentMeta } from '@storybook/react'
 
 import { IntervalWrapper, IntervalWrapperNS, Spin, Title } from '../components'
-import { StoryPlayground } from './components'
+import { CommonStory, StoryPlayground } from './components'
 import { color } from '../utils'
+
+const children = (
+  <Title h1 style={{ color: color({ source: 'text' }) }}>
+    This content showed after the interval passed
+  </Title>
+)
+
+const loader = <Spin />
 
 export default {
   title: 'Utility/Interval wrapper',
@@ -12,15 +20,36 @@ export default {
   args: {
     interval: 1000,
     immediateReloadDelay: 100,
-    children: (
-      <Title h1 style={{ color: color({ source: 'text' }) }}>
-        This content showed after the interval passed
-      </Title>
-    ),
-    loader: <Spin />,
+    children,
+    loader,
     strategy: 'toggle',
   },
 } as ComponentMeta<typeof IntervalWrapper>
+
+export const Strategies = () => {
+  return (
+    <CommonStory
+      component={IntervalWrapper}
+      stories={[
+        {
+          group: [
+            { name: 'Toggle strategy (Default)', props: { children, loader, interval: 1000 } },
+            {
+              name: 'Immediate reload strategy',
+              props: {
+                children,
+                loader,
+                interval: 700,
+                strategy: 'immediate-reload',
+                immediateReloadDelay: 200,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  )
+}
 
 export const Playground: FC<IntervalWrapperNS.Props> = props => {
   return <StoryPlayground component={IntervalWrapper} props={props} />

23974ffc6361518d3c5163fbc124ab5e2e322c35
qafoori <qafoori.hr@gmail.com>
2023-08-09 12:18:16 +0330

    rebuild

--- dist/constants/breakpoints.d.ts
+++ dist/constants/breakpoints.d.ts
@@ -1,8 +1,8 @@
 export declare const BREAKPOINTS: {
-    xs: number;
-    sm: number;
-    md: number;
-    lg: number;
-    xl: number;
-    xxl: number;
-};
+  xs: number
+  sm: number
+  md: number
+  lg: number
+  xl: number
+  xxl: number
+}

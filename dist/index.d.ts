e58667e01bb2e70efe96f54b5045bf81a4175b3a
HamidReza Qafoori <qafoori.hr@gmail.com>
2023-08-10 17:03:30 +0330

    feat(global): Configure dist result

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,6 @@
+import 'react-advanced-cropper/dist/style.css';
+import 'react-advanced-cropper/dist/themes/compact.css';
+import 'react-rangeslider/lib/index.css';
+import 'overlayscrollbars/overlayscrollbars.css';
 import './styles/index.scss';
 export * from './components';
